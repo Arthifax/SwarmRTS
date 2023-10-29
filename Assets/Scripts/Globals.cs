d77f3170af8dad3570b659ab23e5322019f14dee
Arthifax <pepijnlinden@gmail.com>
2023-10-29 03:01:50 +0100

    Added Placeable buildings and TextMeshPro

---
+++ Assets/Scripts/Globals.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Globals
+{
+    public static BuildingData[] BUILDING_DATA = new BuildingData[]
+    {
+        new BuildingData("House", 100),
+        new BuildingData("Tower", 50)
+    };
+
+    public static int TERRAIN_LAYER_MASK = 1 << 8;
+}
